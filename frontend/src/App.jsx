79dac61fd2d10893a672dc3a00f5a51065a7d089
malisettisamrat <samratmalisetti@gmail.com>
2024-09-02 22:34:40 -0700

    created post request functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import { Todos } from "./components/Todos";
 function App() {
   const [todos, setTodos] = useState([]);
 
-  fetch("http://localhost:3000/todo").then(async function (res) {
+  fetch("http://localhost:3000/todos").then(async function (res) {
     const json = await res.json();
     setTodos(json.todos);
   });
--- frontend/src/components/CreateTodo.jsx
+++ frontend/src/components/CreateTodo.jsx
@@ -1,19 +1,49 @@
+import { useState } from "react";
+
 function CreateTodo() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
   return (
     <div>
       <input
         style={{ margin: 10, padding: 10 }}
         type="text"
+        onChange={function (e) {
+          setTitle(e.target.value);
+        }}
         placeholder="Enter title"
       ></input>
       <br />
       <input
         style={{ margin: 10, padding: 10 }}
         type="text"
+        onChange={function (e) {
+          setDescription(e.target.value);
+        }}
         placeholder="Enter description"
       ></input>
       <br />
-      <button style={{ margin: 10, padding: 5 }}>Add a Todo</button>
+      <button
+        style={{ margin: 10, padding: 5 }}
+        onClick={() => {
+          fetch("http://localhost:3000/todo", {
+            method: "POST",
+            body: JSON.stringify({
+              title: title,
+              description: description,
+            }),
+            header: {
+              "Content-type": "application/json",
+            },
+          }).then(async function (res) {
+            const json = await res.json();
+            alert("Todo added");
+          });
+        }}
+      >
+        Add a Todo
+      </button>
       <br />
     </div>
   );
